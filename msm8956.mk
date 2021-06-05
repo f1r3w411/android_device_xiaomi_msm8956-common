c0ef197605785faa590e33b890a8081876e84a4b
f1r3w411 <novikov.firewall@gmail.com>
2021-06-06 02:32:59 +0300

    Added missing fingerprint packages
    Change-Id: I103a2ed127c7b0728468a8458b5908b380e3a37f

--- msm8956.mk
+++ msm8956.mk
@@ -153,7 +153,11 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service \
     fingerprintd
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FM
 PRODUCT_PACKAGES += \
