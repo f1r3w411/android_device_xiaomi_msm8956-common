894c3952b189d3194e11cadcb6ad8df50e50c3c3
f1r3w411 <novikov.firewall@gmail.com>
2021-06-10 16:46:33 +0300

    Replaced FM radio packages
    Change-Id: I3805de17214be3c49cd4a5347225c2e9f9a0aa59

--- msm8956.mk
+++ msm8956.mk
@@ -166,8 +166,10 @@ PRODUCT_COPY_FILES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio \
+    qcom.fmradio.xml
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
